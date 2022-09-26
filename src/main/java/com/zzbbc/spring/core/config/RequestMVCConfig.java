27f2244806d1975f1b71906ac68c0d6c457a977e
zzBBc <vuongngxuan0109@gmail.com>
2022-09-26 15:03:48 +0700

    Add cros

--- src/main/java/com/zzbbc/spring/core/config/RequestMVCConfig.java
+++ src/main/java/com/zzbbc/spring/core/config/RequestMVCConfig.java
@@ -2,6 +2,7 @@ package com.zzbbc.spring.core.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.zzbbc.spring.core.interceptors.RequestInterceptor;
@@ -16,4 +17,11 @@ public class RequestMVCConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestInterceptor);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+
 }
