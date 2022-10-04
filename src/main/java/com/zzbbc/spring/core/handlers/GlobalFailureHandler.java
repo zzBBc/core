af2e524ce9720297bf289f7aeab910ad7462eeef
zzBBc <vuongngxuan0109@gmail.com>
2022-10-04 17:49:58 +0700

    Update Handle failure global

--- src/main/java/com/zzbbc/spring/core/handlers/GlobalFailureHandler.java
+++ src/main/java/com/zzbbc/spring/core/handlers/GlobalFailureHandler.java
@@ -2,11 +2,14 @@ package com.zzbbc.spring.core.handlers;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import com.zzbbc.spring.core.exceptions.BusinessException;
 import com.zzbbc.spring.core.models.BaseResponse;
@@ -21,6 +24,16 @@ public class GlobalFailureHandler extends ResponseEntityExceptionHandler {
 
         return toErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleAsyncRequestTimeoutException(
+            AsyncRequestTimeoutException ex, HttpHeaders arg1, HttpStatus arg2, WebRequest arg3) {
+        LOGGER.error(ex.getMessage(), ex);
+        return toErrorResponse(HttpStatus.REQUEST_TIMEOUT);
+    }
+
+
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handleIllegalArgumentException(Exception ex, WebRequest request) {
@@ -38,7 +51,7 @@ public class GlobalFailureHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.ok(ex.getBaseResponse());
     }
 
-    private ResponseEntity<?> toErrorResponse(HttpStatus httpStatus) {
+    private ResponseEntity<Object> toErrorResponse(HttpStatus httpStatus) {
         return new ResponseEntity<>(new BaseResponse(httpStatus), httpStatus);
     }
 }
